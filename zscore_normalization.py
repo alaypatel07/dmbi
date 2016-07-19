604b6db96a091f138452ab1708272c84657a5089
alaypatel07 <alay1431@gmail.com>
2016-07-19 15:39:08 +0530

    Update zscore_normalization.py to make a testable get_z_score function

--- zscore_normalization.py
+++ zscore_normalization.py
@@ -2,43 +2,33 @@ import csv
 from functools import reduce
 
 
-def mean(records):
-    return sum(records)/len(records)
+def get_z_score(records):
+    # Calculate mean
+    m = sum(records)/len(records)
+    # Calculate standard deviation
+    std_dev = (reduce(lambda x, y: x + ((y - m) ** 2), records, 0) / len(records)) ** 0.5
+    # Calculate Z score
+    z = map(lambda x: (x - m) / std_dev, records)
+    return z
+
 
 if __name__ == '__main__':
-    i = input()
-    if i == "test":
-        records = [1, 2, 3]
-        m = mean(records)
+    with open("Data.csv") as csvfile:
+        data_iter = csv.DictReader(csvfile, delimiter=',', quotechar='"')
+        try:
+            data = [row for row in data_iter]
+            sales = [float(row["Sales"]) for row in data]
 
-        std_dev = (reduce(lambda x, y: x + ((y - m) ** 2), records, 0) / len(records)) ** 0.5
+            # print(sales)
 
-        z = map(lambda x: (x - m) / std_dev, records)
+            z = get_z_score(sales)
 
-        for i in z:
-            print(i)
-    else:
-
-        with open("Data.csv") as csvfile:
-            data_iter = csv.DictReader(csvfile, delimiter=',', quotechar='"')
-            try:
-                data = [row for row in data_iter]
-                sales = [float(row["Sales"]) for row in data]
-
-                # print(sales)
-                # Calculate mean
-                m = mean(sales)
-                # Calculate standard deviation
-                std_dev = (reduce(lambda x, y: x + ((y - m) ** 2), sales, 0) / len(sales)) ** 0.5
-                # Calculate Z score
-                z = map(lambda x: (x - m) / std_dev, sales)
-                # Prepare data structure to write in file
-                new_data = [dict(row, z_score_sales=new_row) for row, new_row in zip(data, z)]
-                # Open file for writing
-                with open("ZNormalizedData.csv", "w") as new_csv_file:
-                    data_writer = csv.DictWriter(new_csv_file, new_data[0].keys())
-                    data_writer.writeheader()
-                    data_writer.writerows(new_data)
-            except TypeError as e:
-                print(e)
-
+            # Prepare data structure to write in file
+            new_data = [dict(row, z_score_sales=new_row) for row, new_row in zip(data, z)]
+            # Open file for writing
+            with open("ZNormalizedData.csv", "w") as new_csv_file:
+                data_writer = csv.DictWriter(new_csv_file, new_data[0].keys())
+                data_writer.writeheader()
+                data_writer.writerows(new_data)
+        except TypeError as e:
+            print(e)
