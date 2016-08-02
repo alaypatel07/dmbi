8eae277a7bac5260e3fa35621d9d110c5a7e3398
alaypatel07 <alay1431@gmail.com>
2016-08-02 23:59:33 +0530

    Update information_gain/entropy.py

--- information_gain/entropy.py
+++ information_gain/entropy.py
@@ -8,15 +8,30 @@ def get_entropy(favourable_outcomes, total_outcomes):
     return - pi * log(pi, 2)
 
 
-def get_outcomes(data, outcome):
-    outcome_set = set(data.get_column(outcome))
-    outcomes = dict(zip(outcome_set, [0] * len(outcome_set)))
+def get_attribute_values(data, attribute):
+    value_set = set(data.get_column(attribute))
+    values = dict(zip(value_set, [0] * len(value_set)))
     for row in data:
-        outcomes[row[outcome]] += 1
-    return outcomes
+        values[row[attribute]] += 1
+    return values
+
+
+class Node(object):
+    def __init__(self, attribute, data):
+        self.attribute = attribute
+        self.data = data
+
+    def get_confidence(self, outcome):
+        values = get_attribute_values(self.data, self.attribute)
+        value_outcomes = {}
+        for value in values:
+
 
 
 if __name__ == '__main__':
     file_name = "dataset.csv"
     data = Extractor(file_name)
-    print(get_outcomes(data))
+    outcomes = get_attribute_values(data, "Outcome")
+    total_outcomes = sum(outcomes.values())
+    for outcome in outcomes:
+        print(get_entropy(outcomes[outcome], total_outcomes))
