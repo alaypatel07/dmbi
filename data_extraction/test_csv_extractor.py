8d7f5af600ea8912af5f58deb05fcc3bfdf32545
alaypatel07 <alay1431@gmail.com>
2016-08-02 13:21:27 +0530

    Update data_extraction/test_csv_extractor.py to test get_column method

--- data_extraction/test_csv_extractor.py
+++ data_extraction/test_csv_extractor.py
@@ -4,9 +4,19 @@ from unittest import TestCase
 
 class TestCsvExtractor(TestCase):
     test_file = "test_data.csv"
-    expected_out = [dict(foo=str(i), bar=str(i + 1), zoo=str(i + 2)) for i in range(1, 10, 3)]
+    expected_init_out = [dict(foo=str(i), bar=str(i + 1), zoo=str(i + 2)) for i in range(1, 10, 3)]
+
+    def setUp(self):
+        self.data = csv_extractor.Extractor(TestCsvExtractor.test_file)
 
     def test_init(self):
-        data = csv_extractor.Extractor(TestCsvExtractor.test_file)
-        for out, expected in zip(data, TestCsvExtractor.expected_out):
+        for out, expected in zip(self.data, TestCsvExtractor.expected_init_out):
             self.assertDictEqual(out, expected)
+
+    def test_get_column(self):
+        column_name = "foo"
+        expected_out = [1, 4, 7]
+        for out, expected in zip(self.data.get_column(column_name), expected_out):
+            self.assertEqual(out, str(expected))
+        column_name += "bar"
+        self.assertRaises(KeyError, self.data.get_column, column_name)
