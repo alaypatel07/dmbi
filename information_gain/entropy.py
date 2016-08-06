d0923797a78ad82132fa49e7d44aa679c051c5c7
alaypatel07 <alay1431@gmail.com>
2016-08-07 01:29:54 +0530

    Update information_gain/entropy.py to add populate_values

--- information_gain/entropy.py
+++ information_gain/entropy.py
@@ -31,10 +31,20 @@ class Data(object):
 
 
 class Node(object):
-    def __init__(self, name, data, attribute=None):
+    def __init__(self, name, data, outcome_column, attribute=None):
         self.name = name
-        self.data = data
+        self.data = Data(data)
         self.attribute = attribute
         self.children = []
         self.flag = False
-        self.values = {}
+        self.values = self.populate_values(attribute)
+        self.outcome_column = outcome_column
+        self.outcomes = self.populate_values(outcome_column)
+
+    def populate_values(self, attribute):
+        if attribute is None:
+            return None
+        else:
+            n =  set(self.data.get_column(attribute))
+            print(n)
+            return n
